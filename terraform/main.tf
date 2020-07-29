b9eef7e6a1ea002d971d6e66d565d91c006534b7
limed <limed@mozilla.com>
2020-07-29 15:29:46 -0700

    Add prometheus

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,17 +13,22 @@ terraform {
 locals {
   project_id   = "mozilla-it-service-engineering"
   cluster_name = "itse-apps-admin-1"
+
+  cluster_features = {
+    "prometheus" = true
+  }
 }
 
 module "gke" {
-  source      = "github.com/mozilla-it/terraform-modules//gcp/gke?ref=master"
-  costcenter  = "1410"
-  environment = "admin"
-  project_id  = local.project_id
-  name        = local.cluster_name
-  region      = var.region
-  regional    = true
-  network     = data.terraform_remote_state.vpc.outputs.network_name
-  subnetwork  = data.terraform_remote_state.vpc.outputs.subnets_names_map[var.region]
+  source           = "github.com/mozilla-it/terraform-modules//gcp/gke?ref=master"
+  costcenter       = "1410"
+  environment      = "admin"
+  project_id       = local.project_id
+  name             = local.cluster_name
+  region           = var.region
+  regional         = true
+  network          = data.terraform_remote_state.vpc.outputs.network_name
+  subnetwork       = data.terraform_remote_state.vpc.outputs.subnets_names_map[var.region]
+  cluster_features = local.cluster_features
 }
 
